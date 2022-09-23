0c0745477be3d486f6bc9d9c5270a4aafab2f32f
sandeepPangare <pangaresandeep96@gmail.com>
2022-09-23 22:18:24 +0530

    As a User need to

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,8 +1,16 @@
 package com.bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class UserRegistration {
     public static void main(String[] args) {
+        /*First name starts with Cap and has
+        minimum 3 characters*/
 
+        Pattern pattern = Pattern.compile("^[A-Z]"+"[a-z]{3,}");
+        Matcher matcher = pattern.matcher("Sandeep");
+        boolean result = matcher.matches();
 
+        System.out.println("Result = " +result);
     }
 }
