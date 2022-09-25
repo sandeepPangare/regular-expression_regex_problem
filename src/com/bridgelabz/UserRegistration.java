e0df6b0358524b0b371387692ca95fabf552c39b
sandeepPangare <pangaresandeep96@gmail.com>
2022-09-25 16:12:30 +0530

    Added:Valid PhoneNumber

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -15,8 +15,11 @@ public class UserRegistration {
         /* E.g. abc.xyz@bl.co.in -
         Email has 3 mandatory parts (abc, bl& co) and 2 optional (xyz & in) with
         precise @ and . positions */
-        boolean email = Pattern.matches("[abc]{1,3}" + "[.]" + "[a-z 0-9]{1,}" + "[@]" + "bl{1}" + "[.]" + "co{1}" + "[.]" + "[a-z]{1,}", "abc.xyz@bl.co.in");
+        boolean email = Pattern.matches("[abc]{1,3}"+"[.]"+"[a-z 0-9]{1,}"+"[@]"+"bl{1}"+"[.]"+"co{1}"+"[.]"+"[a-z]{1,}", "abc.xyz@bl.co.in");
         System.out.println("Result = "+email );
+        // E.g. 91 9919819801-Country code follow by space and 10digit number
+        boolean PhoneNumber = Pattern.matches("^91{1}\s[0-9]{10}", "91 9917819801");
+        System.out.println("Result = "+PhoneNumber);
 
     }
 }
